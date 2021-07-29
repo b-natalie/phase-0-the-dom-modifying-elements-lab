4dce08114deb40f19d0cfd61d1f1a4a3b5cbcba2
Natalie Barba <nbarba2010@gmail.com>
2021-07-28 18:27:58 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const removeMain = document.getElementById('main');
+removeMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader); 
+
+newHeader.innerHTML = 'Natalie is the champion'
